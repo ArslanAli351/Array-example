411ad8a1fd8342e501e7878aa5d3c71fa13fb6d1
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-27 17:25:52 -0700

    havascript example

---
+++ app.js
@@ -0,0 +1,45 @@
+// let number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,]
+// console.log(number.slice(11,19))
+
+
+
+
+
+
+
+
+//  let cricketers = ["Afridi", "Misbah", "Dhoni", "Yovraj", "Shoaib Akhtar", "Muhammad Asif", "Tendulkar", "Sehwag"];
+// console.log(cricketers.slice(0,2,))
+
+
+
+
+
+
+
+
+
+
+let na=["A","r","s","l","a","n","A","l","i"]
+document.write(na.join(""))
+
+
+// let furit=["B","a","n","a","n","a"]
+// console.log(furit.join(""))
+
+
+
+// let na=["A","r","s","l","a","n","A","l","i"]
+// document.write(na.reverse())
+
+
+// let firstName = 'Arslan Ali';
+
+// console.log(firstName[7]);
+
+// console.log(firstName.length);
+
+// console.log(firstName.slice(7, 10));
+
+
+
